6505cac52a6ce8940b0256ed65b0ad3326e2ff66
Rommel Villagomez <axlrommel@users.noreply.github.com>
2015-09-01 04:53:26 +0000

    added bird set to redis

--- whatIsThisBird/src/main/java/com/villagomezdiaz/utilities/ImageComparer.java
+++ whatIsThisBird/src/main/java/com/villagomezdiaz/utilities/ImageComparer.java
@@ -1,11 +1,10 @@
 package com.villagomezdiaz.utilities;
 
-import static java.nio.file.StandardCopyOption.REPLACE_EXISTING;
-
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.PrintWriter;
 import java.nio.file.Files;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
@@ -15,31 +14,43 @@ import com.jhlabs.image.ImageUtils;
 import com.villagomezdiaz.common.ColorBlackBackgroundFilter;
 import com.villagomezdiaz.common.ImageCorrelation;
 import com.villagomezdiaz.common.ImageStatistics;
+import com.villagomezdiaz.common.ds.BirdMatchingResults;
+import com.villagomezdiaz.common.ds.BirdsResults;
+import com.villagomezdiaz.common.ds.FilterResults;
 
 import redis.clients.jedis.Jedis;
 
 public class ImageComparer {
 
 	static PrintWriter writer = null;
-	static final int maxMatches = 7;
+	static final int maxMatches = 10;
+	static final double threshold = 0.60;
+	private static String outputHttpPath = "http://localhost/images/input/";
+	private static String outputSystemPath = "/var/www/html/images/input/";
 	
 	public static void main(String[] args) {
+		ImageComparer ic = new ImageComparer();
+		BirdMatchingResults bc = ic.getMatchingResults(args[0]);
+		System.out.println(bc.toString());
+	}
 		
-		String outputHttpPath = "http://localhost/images/input/";
-		String outputSystemPath = "/var/www/html/images/input/";
+	
+	public BirdMatchingResults getMatchingResults(String inputBird) {
 		
+		BirdMatchingResults results = new BirdMatchingResults();
 		Jedis jedis = new Jedis("localhost");
-		
+				
 		try {
 					
-			File input = new File(args[0]);
+			File input = new File(inputBird);
 			String fname1 = input.getName();
 			File cloneOutput = new File(outputSystemPath + fname1);
 			
 			 BufferedImage imageIn = ImageIO.read(input);
 			 
 			 // copy the image
-			 Files.copy(input.toPath(), cloneOutput.toPath(), REPLACE_EXISTING);
+			 Files.copy(input.toPath(), cloneOutput.toPath(), 
+					 java.nio.file.StandardCopyOption.REPLACE_EXISTING);
 			 
 			 //using PosixFilePermission to set file permissions 755
 //		     FilePermissions.setFilePermissions(cloneOutput.toPath());
@@ -61,11 +72,14 @@ public class ImageComparer {
 			 File output = new File(outputSystemPath + "T" + fname1);
 			 ImageIO.write(imageTmp1, "jpg", output);
 			 
+			 Long numSpecies = jedis.scard("birds");
+				
 			 List<String> list1 = jedis.lrange("images", 0 ,-1);
+			 int numImages = list1.size();
 		     for(int i=0; i<list1.size(); i++) {
-		    	 String redList = "rr-" + list1.get(i);
-		    	 String greenList = "gg-" + list1.get(i);
-		    	 String blueList = "bb-" + list1.get(i);
+		    	 String redList = "r-" + list1.get(i);
+		    	 String greenList = "g-" + list1.get(i);
+		    	 String blueList = "b-" + list1.get(i);
 		    	 
 		    	 List<String> listR = jedis.lrange(redList, 0,-1);
 		    	 List<String> listG = jedis.lrange(greenList, 0,-1);
@@ -77,53 +91,53 @@ public class ImageComparer {
 				 
 				 ImageCorrelation corr = new ImageCorrelation(inputStat,fStat);
 
-				 if(corr.getThreeColorCorrelation() > 0.6) {
+				 if(corr.getThreeColorCorrelation() > threshold) {
 					 jedis.zadd("threecolor-corr", corr.getThreeColorCorrelation(), list1.get(i));
 				 }
 				 
-				 if(corr.getLowColorCorrelation() > 0.6) {
+				 if(corr.getLowColorCorrelation() > threshold) {
 					 jedis.zadd("low-corr", corr.getLowColorCorrelation(), list1.get(i));
 				 }
 				 
-				 if(corr.getHighColorCorrelation() > 0.6) {
+				 if(corr.getHighColorCorrelation() > threshold) {
 					 jedis.zadd("high-corr", corr.getHighColorCorrelation(), list1.get(i));
 				 }
 				 
-				 if(corr.getRedColorCorrelation() > 0.6) {
+				 if(corr.getRedColorCorrelation() > threshold) {
 					 jedis.zadd("red-corr", corr.getRedColorCorrelation(), list1.get(i));
 				 }
 				 
-				 if(corr.getGreenColorCorrelation() > 0.6) {
+				 if(corr.getGreenColorCorrelation() > threshold) {
 					 jedis.zadd("green-corr", corr.getGreenColorCorrelation(), list1.get(i));
 				 }
 				 
-				 if(corr.getBlueColorCorrelation() > 0.6) {
+				 if(corr.getBlueColorCorrelation() > threshold) {
 					 jedis.zadd("blue-corr", corr.getBlueColorCorrelation(), list1.get(i));
 				 }
 				 
-				 if(corr.getYellowColorCorrelation() > 0.6) {
+				 if(corr.getYellowColorCorrelation() > threshold) {
 					 jedis.zadd("yellow-corr", corr.getYellowColorCorrelation(), list1.get(i));
 				 }
 				 
-				 if(corr.getCyanColorCorrelation() > 0.6) {
+				 if(corr.getCyanColorCorrelation() > threshold) {
 					 jedis.zadd("cyan-corr", corr.getCyanColorCorrelation(), list1.get(i));
 				 }
 				 
-				 if(corr.getMagentaColorCorrelation() > 0.6) {
+				 if(corr.getMagentaColorCorrelation() > threshold) {
 					 jedis.zadd("magenta-corr", corr.getMagentaColorCorrelation(), list1.get(i));
 				 }
 			 }
 		     
 		     // let's get the top 6 for each, remove the rest
-		     jedis.zremrangeByRank("threecolor-corr", 0, -6);
-		     jedis.zremrangeByRank("low-corr", 0, -6);
-		     jedis.zremrangeByRank("high-corr", 0, -6);
-		     jedis.zremrangeByRank("red-corr", 0, -6);
-		     jedis.zremrangeByRank("green-corr", 0, -6);
-		     jedis.zremrangeByRank("blue-corr", 0, -6);
-		     jedis.zremrangeByRank("yellow-corr", 0, -6);
-		     jedis.zremrangeByRank("cyan-corr", 0, -6);
-		     jedis.zremrangeByRank("magenta-corr", 0, -6);
+		     jedis.zremrangeByRank("threecolor-corr", 0, -maxMatches + 1);
+		     jedis.zremrangeByRank("low-corr", 0, -maxMatches + 1);
+		     jedis.zremrangeByRank("high-corr", 0,  -maxMatches + 1);
+		     jedis.zremrangeByRank("red-corr", 0,  -maxMatches + 1);
+		     jedis.zremrangeByRank("green-corr", 0,  -maxMatches + 1);
+		     jedis.zremrangeByRank("blue-corr", 0,  -maxMatches + 1);
+		     jedis.zremrangeByRank("yellow-corr", 0,  -maxMatches + 1);
+		     jedis.zremrangeByRank("cyan-corr", 0,  -maxMatches + 1);
+		     jedis.zremrangeByRank("magenta-corr", 0,  -maxMatches + 1);
 			 
 		     // union all, will add the scores if on two or more keys
 		     jedis.zunionstore("all-corr", "threecolor-corr","low-corr","high-corr","red-corr","green-corr","blue-corr","yellow-corr",
@@ -131,9 +145,67 @@ public class ImageComparer {
 		     
 		     // let's print out the unique ones and its score
 		     Set<String> qq = jedis.zrevrange("all-corr", 0, -1);
+		     Set<BirdsResults> bResults = new HashSet<BirdsResults>();
 		     for(String s : qq) {
-		    	 System.out.println(s + " " + jedis.zscore("all-corr", s));
+		    	 Set<FilterResults> fResults = new HashSet<FilterResults>();
+		    	 System.out.println(s + " all-corr " + jedis.zscore("all-corr", s));
+		    	 if(jedis.zscore("threecolor-corr",s) != null) {
+		    		 FilterResults fr = new FilterResults("threecolor-corr",jedis.zscore("threecolor-corr", s));
+		    		 fResults.add(fr);
+		    		 System.out.println("\t" + s + " threecolor-corr " + jedis.zscore("threecolor-corr", s));
+		    	 }
+		    	 if(jedis.zscore("low-corr",s) != null) {
+		    		 FilterResults fr = new FilterResults("low-corr",jedis.zscore("low-corr", s));
+		    		 fResults.add(fr);
+		    		 System.out.println("\t" + s + " low-corr " + jedis.zscore("low-corr", s));
+		    	 }
+		    	 if(jedis.zscore("high-corr",s) != null) {
+		    		 FilterResults fr = new FilterResults("high-corr",jedis.zscore("high-corr", s));
+		    		 fResults.add(fr);
+		    		 System.out.println("\t" + s + " high-corr " + jedis.zscore("high-corr", s));
+		    	 }
+		    	 if(jedis.zscore("red-corr",s) != null) {
+		    		 FilterResults fr = new FilterResults("red-corr",jedis.zscore("red-corr", s));
+		    		 fResults.add(fr);
+		    		 System.out.println("\t" + s + " red-corr " + jedis.zscore("red-corr", s));
+		    	 }
+		    	 if(jedis.zscore("green-corr",s) != null) {
+		    		 FilterResults fr = new FilterResults("green-corr",jedis.zscore("green-corr", s));
+		    		 fResults.add(fr);
+		    		 System.out.println("\t" + s + " green-corr " + jedis.zscore("green-corr", s));
+		    	 }
+		    	 if(jedis.zscore("blue-corr",s) != null) {
+		    		 FilterResults fr = new FilterResults("blue-corr",jedis.zscore("blue-corr", s));
+		    		 fResults.add(fr);
+		    		 System.out.println("\t" + s + " blue-corr " + jedis.zscore("blue-corr", s));
+		    	 }
+		    	 if(jedis.zscore("yellow-corr",s) != null) {
+		    		 FilterResults fr = new FilterResults("yellow-corr",jedis.zscore("yellow-corr", s));
+		    		 fResults.add(fr);
+		    		 System.out.println("\t" + s + " yellow-corr " + jedis.zscore("yellow-corr", s));
+		    	 }
+		    	 if(jedis.zscore("cyan-corr",s) != null) {
+		    		 FilterResults fr = new FilterResults("cyan-corr",jedis.zscore("cyan-corr", s));
+		    		 fResults.add(fr);
+		    		 System.out.println("\t" + s + " cyan-corr " + jedis.zscore("cyan-corr", s));
+		    	 }
+		    	 if(jedis.zscore("magenta-corr",s) != null) {
+		    		 FilterResults fr = new FilterResults("magenta-corr",jedis.zscore("magenta-corr", s));
+		    		 fResults.add(fr);
+		    		 System.out.println("\t" + s + " magenta-corr " + jedis.zscore("magenta-corr", s));
+		    	 }
+		    	 
+		    	 int p = s.indexOf(".");
+		    	 int p1 = s.indexOf("/");
+		    	 String birdName = s.substring(p + 1, p1);
+		    	 String b = birdName.replaceAll("_", " ");
+		    	 BirdsResults br = new BirdsResults(b, s, jedis.zscore("all-corr", s), fResults);
+		    	 bResults.add(br);
 		     }
+		     
+		     results.setNumSpecies(numSpecies);
+		     results.setNumBirds(numImages);
+		     results.setbResults(bResults);
 		     
 		     // let's remove the keys
 		     jedis.del("all-corr", "threecolor-corr","low-corr","high-corr","red-corr","green-corr","blue-corr","yellow-corr",
@@ -146,10 +218,9 @@ public class ImageComparer {
 		finally {
 			jedis.close();
 		}
-
+		return results;
+		
 	}
-	
-	
 
 }
 
