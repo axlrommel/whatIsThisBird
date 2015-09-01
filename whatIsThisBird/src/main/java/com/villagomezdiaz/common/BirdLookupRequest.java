62447ac1926f8484e5dccd1fb4ff7e43a095df1a
axlrommel <axlrommel@36879ab6-8312-4cd2-b3e7-bb425396280f>
2015-09-01 05:06:37 +0000

    initial checkin

---
+++ whatIsThisBird/src/main/java/com/villagomezdiaz/common/BirdLookupRequest.java
@@ -0,0 +1,21 @@
+package com.villagomezdiaz.common;
+
+public class BirdLookupRequest {
+
+	private String path;
+
+	public BirdLookupRequest(String path) {
+		super();
+		this.path = path;
+	}
+
+	public String getPath() {
+		return path;
+	}
+
+	public void setPath(String path) {
+		this.path = path;
+	}
+	
+	
+}
