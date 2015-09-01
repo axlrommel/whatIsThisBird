06f3a6bf105d868df85b6748ff64d3e74effd1a7
axlrommel <axlrommel@36879ab6-8312-4cd2-b3e7-bb425396280f>
2015-09-01 05:07:23 +0000

    changed to return JSON object for BirdsLookup

--- whatIsThisBird/src/main/java/com/villagomezdiaz/app/JSONServlet.java
+++ whatIsThisBird/src/main/java/com/villagomezdiaz/app/JSONServlet.java
@@ -3,31 +3,27 @@ package com.villagomezdiaz.app;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.LinkedList;
-import java.util.List;
+
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
- 
+
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.villagomezdiaz.app.vo.Article;
+import com.villagomezdiaz.common.BirdLookupRequest;
+import com.villagomezdiaz.common.ds.BirdMatchingResults;
+import com.villagomezdiaz.utilities.ImageComparer;
+
  
 public class JSONServlet extends HttpServlet {
  
     private static final long serialVersionUID = 1L;
  
-    // This will store all received articles
-    List<Article> articles = new LinkedList<Article>();
  
-    /***************************************************
-     * URL: /jsonservlet
-     * doPost(): receives JSON data, parse it, map it and send back as JSON
-     ****************************************************/
-    protected void doPost(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException{
- 
-        // 1. get received JSON data from request
+    @Override
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) 
+			throws ServletException, IOException {
+    	// 1. get received JSON data from request
         BufferedReader br = new BufferedReader(new InputStreamReader(request.getInputStream()));
         String json = "";
         if(br != null){
@@ -37,16 +33,19 @@ public class JSONServlet extends HttpServlet {
         // 2. initiate jackson mapper
         ObjectMapper mapper = new ObjectMapper();
  
-        // 3. Convert received JSON to Article
-        Article article = mapper.readValue(json, Article.class);
+        // 3. Convert received JSON to BirdLookupRequest
+        BirdLookupRequest req = mapper.readValue(json, BirdLookupRequest.class);
  
         // 4. Set response type to JSON
         response.setContentType("application/json");            
  
-        // 5. Add article to List<Article>
-        articles.add(article);
+        // 5. create object to return
+        ImageComparer ic = new ImageComparer();
+        BirdMatchingResults bmr = ic.getMatchingResults(req.getPath());
+
  
-        // 6. Send List<Article> as JSON to client
-        mapper.writeValue(response.getOutputStream(), articles);
-    }
+        // 6. Send BirdMatchingResults as JSON to client
+        mapper.writeValue(response.getOutputStream(), bmr);
+	}
+
 }
