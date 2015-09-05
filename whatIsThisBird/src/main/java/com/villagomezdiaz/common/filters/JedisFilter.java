1460bfad691b76e18dd8b153dad44e4348cd81d5
Rommel Villagomez <axlrommel@users.noreply.github.com>
2015-09-05 04:47:26 +0000

    initial checkin

---
+++ whatIsThisBird/src/main/java/com/villagomezdiaz/common/filters/JedisFilter.java
@@ -0,0 +1,21 @@
+package com.villagomezdiaz.common.filters;
+
+public class JedisFilter {
+	private String filterName;
+
+	public JedisFilter(String filterName) {
+		super();
+		this.filterName = filterName;
+	}
+
+	public String getFilterName() {
+		return filterName;
+	}
+
+	public void setFilterName(String filterName) {
+		this.filterName = filterName;
+	}
+	
+	
+
+}
