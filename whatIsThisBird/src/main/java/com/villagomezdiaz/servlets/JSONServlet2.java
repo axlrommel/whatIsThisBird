5cd8327b661398d128e8ffe80d2f17844f82630e
axlrommel <axlrommel@36879ab6-8312-4cd2-b3e7-bb425396280f>
2015-09-05 06:39:26 +0000

    rearranged package

---
+++ whatIsThisBird/src/main/java/com/villagomezdiaz/servlets/JSONServlet2.java
@@ -0,0 +1,49 @@
+package com.villagomezdiaz.servlets;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.villagomezdiaz.common.ds.BirdAndSource;
+
+@WebServlet(name="/JSONServlet2") 
+public class JSONServlet2 extends HttpServlet {
+ 
+    private static final long serialVersionUID = 1L;
+ 
+ 
+    @Override
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) 
+			throws ServletException, IOException {
+ 
+        // 2. initiate jackson mapper
+        ObjectMapper mapper = new ObjectMapper();
+ 
+ 
+        // 4. Set response type to JSON
+        response.setContentType("application/json");            
+ 
+        
+        String buttonID = request.getParameter("button-id");
+        switch (buttonID) {
+        case "info":
+        	mapper.writeValue(response.getOutputStream(), getBirdsAndSources());
+        	break;
+        }
+	}
+    
+    private BirdAndSource getBirdsAndSources() {
+    	
+    	BirdAndSource result = new BirdAndSource("http://www.caltech.com","cardinal");
+    	
+    	return result;
+    		
+    	
+    }
+
+}
