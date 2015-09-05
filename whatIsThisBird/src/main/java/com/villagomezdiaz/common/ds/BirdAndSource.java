c9ad42db86380505f8dc8a913290925e55715e4e
Rommel Villagomez <axlrommel@users.noreply.github.com>
2015-09-05 02:46:59 +0000

    initial checkin

---
+++ whatIsThisBird/src/main/java/com/villagomezdiaz/common/ds/BirdAndSource.java
@@ -0,0 +1,26 @@
+package com.villagomezdiaz.common.ds;
+
+public class BirdAndSource {
+	private String source;
+	private String bird;
+	public String getSource() {
+		return source;
+	}
+	public void setSource(String source) {
+		this.source = source;
+	}
+	public String getBird() {
+		return bird;
+	}
+	public void setBird(String bird) {
+		this.bird = bird;
+	}
+	public BirdAndSource(String source, String bird) {
+		super();
+		this.source = source;
+		this.bird = bird;
+	}
+	
+	
+
+}
